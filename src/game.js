fcf13d8a2e6018892a42795caa6f3f92ef8f0213
KiKiKi <chaika.design@gmail.com>
2020-04-23 14:11:08 +0900

    :gem: Change Draw (all same values) logic.
    if all player same value, result array length become 1.

--- src/game.js
+++ src/game.js
@@ -1,23 +1,22 @@
-import { HANDS, HANDS_ID } from './congig';
+import { HANDS_ID } from './congig';
 
-const isAllSameValue = (arr) => (playerNum) => {
-  console.log(Math.max(...arr), playerNum);
-  return Math.max(...arr) === playerNum;
+const isAllSameValue = (arr) => {
+  return arr.length === 1;
 }
 
 // All same value || Has All Values => DRAW
 export const isDraw = (data) => {
-  const playerNum = data.length;
   const values = data.reduce((arr, player) => {
     if (!arr[HANDS_ID[player.value]]) {
       arr[HANDS_ID[player.value]] = 0;
     }
     arr[HANDS_ID[player.value]] += 1
     return [...arr];
-  }, Array(HANDS.length).fill(0));
+  }, []).filter(Boolean);
+
   console.log(values);
 
-  if (isAllSameValue(values)(playerNum)) {
+  if (isAllSameValue(values)) {
     console.log('Draw! All same value.');
     return true;
   }
