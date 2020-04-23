75c94917235581e918ad65800a559cf69e1dc7b4
KiKiKi <chaika.design@gmail.com>
2020-04-23 13:14:38 +0900

    Add match method, which generate rivals value when player select value.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import { HANDS } from './congig';
 import Rival from './Rival';
 import Controller from './components/Controller';
+
+function getRivalsValues(rivals) {
+  return rivals.map((raival) => raival.getRoll());
+}
 
 export default function App() {
   const [rivals, setRivals] = useState([new Rival(0)]);
 
+  const onMatch = useCallback(
+    (myRoll) => (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      const values = [myRoll, ...getRivalsValues(rivals)];
+      console.log(values);
+    },
+    [rivals]
+  );
+
+  const onStone = useCallback(onMatch(HANDS[0]), [onMatch]);
+  const onScissors = useCallback(onMatch(HANDS[1]), [onMatch]);
+  const onPaper = useCallback(onMatch(HANDS[2]), [onMatch]);
+
   return (
     <>
-      <Controller />
+      <Controller onStone={onStone} onScissors={onScissors} onPaper={onPaper} />
     </>
   );
 }
--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { IconPaper, IconScissors, IconStone } from './Icons';
 
-export default function Controller() {
+export default function Controller({ onStone, onScissors, onPaper }) {
   return (
     <nav className="controller">
       <div className="controll-item">
-        <button className="controll-btn controll-btn--stone">
+        <button className="controll-btn controll-btn--stone" onClick={onStone}>
           <IconStone />
         </button>
       </div>
       <div className="controll-item">
-        <button className="controll-btn controll-btn--scissors">
+        <button className="controll-btn controll-btn--scissors" onClick={onScissors}>
           <IconScissors />
         </button>
       </div>
       <div className="controll-item">
-        <button className="controll-btn controll-btn--paper">
+        <button className="controll-btn controll-btn--paper" onClick={onPaper}>
           <IconPaper />
         </button>
       </div>
