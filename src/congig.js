fb0a8304e079c9257c92b77dc474929bcadb2dcd
KiKiKi <chaika.design@gmail.com>
2020-04-23 13:14:51 +0900

    Add game config

---
+++ src/congig.js
@@ -0,0 +1,10 @@
+export const FIRST_VALUE = 100;
+export const GAME_COST = 10;
+
+export const HANDS = Object.freeze(['STONE', 'SCISSORS', 'PAPER']);
+
+export const HANDS_ID = Object.freeze({
+  [HANDS[0]]: 0,
+  [HANDS[1]]: 1,
+  [HANDS[2]]: 2,
+});
