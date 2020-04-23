43c6d949eb7a0a2e574a48c45cff89dbd0f7dcba
KiKiKi <chaika.design@gmail.com>
2020-04-23 23:16:38 +0900

    Use config variable.

--- src/components/GameIntroCover.jsx
+++ src/components/GameIntroCover.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GAME_COST, MAX_MATCH } from '../congig';
 import PlayBtn from './PlayBtn';
 
 export default function GameIntroCover({ onPlay }) {
@@ -9,15 +10,17 @@ export default function GameIntroCover({ onPlay }) {
       </div>
       <div className="intro-description">
         <ul className="intro-description__list">
-          <li>1 game 10 coin.</li>
-          <li>Game end when 30 matches or coin become 0.</li>
+          <li>1 game {GAME_COST} coin.</li>
+          <li>Game end when {MAX_MATCH} matches or coin become 0.</li>
           <li>can more bet next game, if you win.</li>
           <li>Win: get beted coins 2x!</li>
           <li>Lose: lost bet coins.</li>
           <li>Draw: lose half of beted coins.</li>
         </ul>
       </div>
-      <PlayBtn className="intro-start-btn" onPlay={onPlay}>Play</PlayBtn>
+      <PlayBtn className="intro-start-btn" onPlay={onPlay}>
+        Play
+      </PlayBtn>
     </div>
   );
 }
