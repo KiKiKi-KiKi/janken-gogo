062aebd3c562cace1b59d50390a8f3806f6eadd6
KiKiKi <chaika.design@gmail.com>
2020-04-23 11:17:04 +0900

    :sparkles: App base.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import 'normalize.css';
+import './styles/app.scss';
 
-ReactDOM.render('MyApp', document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
