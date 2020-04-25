3b6622b6d56b1a3c95f491039be2e2c8683d1909
KiKiKi <chaika.design@gmail.com>
2020-04-26 02:39:35 +0900

    Display final match result in game over cover.

--- src/components/GameOverCover.jsx
+++ src/components/GameOverCover.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getResultLabel } from '../game';
 import PlayBtn from './PlayBtn';
 import GameMeta from './GameMeta';
 import { ReactComponent as IconCoins } from '../img/icon-coins.svg';
@@ -7,17 +8,23 @@ function highScore(isHighScore) {
   if (!isHighScore) {
     return null;
   }
-  return <span className="result-hight-score">Hight Score</span>;
+  return <span className="result-hight-score">\ Hight Score /</span>;
 }
 
-export default function GameOverCover({ onPlay, score, match, win, lose, draw, isHighScore }) {
+export default function GameOverCover({ onPlay, score, match, win, lose, draw, result, addScore, isHighScore }) {
+  const resLabel = getResultLabel(result);
   return (
     <div className="board-cover game-over">
       <div className="board-cover__label">
         <b className="board-cover__title">GAME OVER!</b>
+        <div className="final-match-result">
+          Final match:
+          <span className={`result__title ${resLabel}`}>{resLabel}</span>
+          <span className="result__score">(+{addScore})</span>
+        </div>
         {highScore(isHighScore)}
         <p className="final-score">
-          <IconCoins /> {score}
+          <small>Total:</small> <IconCoins /> {score}
         </p>
       </div>
       <div className="game-result">
